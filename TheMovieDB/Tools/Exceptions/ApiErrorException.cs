6e5892c96c0327839e13251f96a35233d86de851
Carlos Figueroa <alex_bazan_1997@hotmail.com>
2021-09-10 22:44:21 -0500

    Feature - Added Helpers and exceptions

---
+++ TheMovieDB/Tools/Exceptions/ApiErrorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace TheMovieDB.Tools.Exceptions
+{
+    public class ApiErrorException : Exception
+    {
+        #region Public Properties
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string StaticMessage { get; set; }
+
+        #endregion
+
+
+        #region Constructor
+
+        public ApiErrorException()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ TheMovieDB/Tools/Helpers/Resources.cs
@@ -0,0 +1,33 @@
+﻿using TheMovieDB.Services.Localization.Interfaces;
+using TheMovieDB.Utils.ServiceLocator;
+
+namespace TheMovieDB.Tools.Helpers
+{
+    public static class Resources
+    {
+        #region Private Properties
+
+        private static ILocalizationService _localizationService =>
+               ServiceLocatorProvider
+               .Instance
+               .GetService<ILocalizationService>();
+
+        #endregion
+
+        #region Public Properties
+
+        public static string NoInternetConnection = GetResources("NoInternetConnection");
+        public static string UnexpectedExceptionOcurred = GetResources("UnexpectedExceptionOcurred");
+
+        #endregion
+
+        #region Private Methods
+
+        private static string GetResources(string key)
+        {
+            return _localizationService.GetResource(key);
+        }
+
+        #endregion
+    }
+}
