3e2b6f73998c94c572adbef395ea4e2dc2f9d0c1
Carlos Figueroa <alex_bazan_1997@hotmail.com>
2021-09-10 22:44:56 -0500

    Feature - App file

--- TheMovieDB/App.xaml.cs
+++ TheMovieDB/App.xaml.cs
@@ -1,17 +1,45 @@
-﻿using System;
+﻿using TheMovieDB.Services.Api.Implementations;
+using TheMovieDB.Services.Api.Interfaces;
+using TheMovieDB.Services.Connection.Implementations;
+using TheMovieDB.Services.Connection.Interfaces;
+using TheMovieDB.Services.Crash.Implementations;
+using TheMovieDB.Services.Crash.Interfaces;
+using TheMovieDB.Services.Localization.Implementations;
+using TheMovieDB.Services.Localization.Interfaces;
+using TheMovieDB.Services.Navigation.Implementations;
+using TheMovieDB.Services.Navigation.Interfaces;
+using TheMovieDB.Utils.ServiceLocator;
+using TheMovieDB.ViewModels.Detail;
+using TheMovieDB.ViewModels.Home;
+using TheMovieDB.Views.Detail;
+using TheMovieDB.Views.Home;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace TheMovieDB
 {
     public partial class App : Application
     {
+        #region Private Properties
+
+        private IServiceLocator _serviceLocator;
+        private INavigationService _navigationService;
+
+        private static bool _navigationInitialized;
+
+        #endregion
+
+        #region Constructor
+
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            InitializeDependencyContainer();
+            InitNavigation();
         }
+
+        #endregion
+
+        #region Protected Methods
 
         protected override void OnStart()
         {
@@ -24,5 +52,64 @@ namespace TheMovieDB
         protected override void OnResume()
         {
         }
+
+        #endregion
+
+        #region Private Methods
+
+        private void InitializeDependencyContainer()
+        {
+            _serviceLocator = ServiceLocatorProvider.Instance.Current;
+            RegisterDependencies();
+            RegisterViewModels();
+            _serviceLocator.Init();
+        }
+
+        private void RegisterViewModels()
+        {
+            _serviceLocator.Register<MovieListPageViewModel>();
+            _serviceLocator.Register<MovieDetailPageViewModel>();
+        }
+
+        private void RegisterDependencies()
+        {
+            _serviceLocator.RegisterSingle<ILocalizationService, LocalizationService>();
+            _serviceLocator.RegisterSingle<IConnectivityService, ConnectivityService>();
+            _serviceLocator.RegisterSingle<ICrashReporting, CrashReporting>();
+            _serviceLocator.RegisterSingle<IApiClient, ApiClient>();
+            _serviceLocator.RegisterSingle<IMovieService, MovieService>();
+            _serviceLocator.RegisterSingle<INavigationService, NavigationService>();
+        }
+
+        private void BindViewsAndViewModels()
+        {
+            // Register all the view models with their corresponding pages
+            _navigationService.RegisterViewMapping(typeof(MovieListPageViewModel), typeof(MovieListPage));
+            _navigationService.RegisterViewMapping(typeof(MovieDetailPageViewModel), typeof(MovieDetailPage));
+        }
+
+        private void InitNavigation()
+        {
+            _navigationService = _navigationService ?? _serviceLocator.Resolve<INavigationService>();
+
+            lock (typeof(App))
+            {
+                if (_navigationInitialized)
+                    return;
+
+                _navigationInitialized = true;
+            }
+
+            BindViewsAndViewModels();
+
+            NavigationToFirstViewModel();
+        }
+
+        private void NavigationToFirstViewModel()
+        {
+            _navigationService.SetNewNavigationPage<MovieListPageViewModel>();
+        }
+
+        #endregion
     }
 }
