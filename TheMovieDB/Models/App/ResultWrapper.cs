2c68db1086827dbd8ee5e84bbd531f2f9bf91737
Carlos Figueroa <alex_bazan_1997@hotmail.com>
2021-09-11 01:19:51 -0500

    Feature - Create ResultWrapper

---
+++ TheMovieDB/Models/App/ResultWrapper.cs
@@ -0,0 +1,11 @@
+﻿using TheMovieDB.Models.Api.Base;
+
+namespace TheMovieDB.Models.App
+{
+    public class ResultWrapper
+    {
+        public Result Model { get; set; }
+
+        public string Image { get; set; }
+    }
+}
